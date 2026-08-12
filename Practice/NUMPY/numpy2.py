1bcf877aa86560b002021452b30987abceea7411
prakayushmsruas-hub <prakayush.msruas@gmail.com>
2026-08-12 10:19:50 +0530

    numpy2

---
+++ Practice/NUMPY/numpy2.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+numbers=np.array([10, 20, 30, 40, 50, 60, 70])
+print(numbers[0])
+print(numbers[-1])
+print(numbers[2])
+print(numbers[1:5])
+print(numbers[:3])
+print(numbers[-3:])
+print(numbers[::2])
